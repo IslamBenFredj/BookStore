0fb31fe65ac72b773f9c34cbe1d657de56b95af5
Islem <islamnov94bf@gmail.com>
2025-10-13 14:20:44 +0200

    first commit - architecture globale de ma solution BookStore

---
+++ BookStore.Core/Models/Book.cs
@@ -0,0 +1,11 @@
+namespace BookStore.Core.Models;
+
+public class Book
+{
+public Guid Id { get; set; } = Guid.NewGuid();
+public string Title { get; set; } = string.Empty;
+public Guid AuthorId { get; set; }
+public DateTime Published { get; set; }
+public string Genre { get; set; } = string.Empty;
+public decimal Price { get; set; }
+}
